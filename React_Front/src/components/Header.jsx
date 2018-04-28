fde81125c535cb23fd4ef6a041431d2978508e7b
Kento75 <kentonet75@gmail.com>
2018-04-28 20:39:36 +0900

    ページングの実装

--- React_Front/src/components/Header.jsx
+++ React_Front/src/components/Header.jsx
@@ -1,21 +1,55 @@
 import React, { PropTypes } from 'react';
+import { Link } from 'react-router';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
-
 import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import Toggle from 'material-ui/Toggle';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import NavigationClose from 'material-ui/svg-icons/navigation/close';
 
 const muiTheme = getMuiTheme();
+
+
+const AppBarIconMenu = (props) => (
+  <AppBar
+    title="Title"
+    iconElementLeft={
+      <IconMenu
+      iconButtonElement={
+        <IconButton><MoreVertIcon /></IconButton>
+      }
+      targetOrigin={{horizontal: 'right', vertical: 'top'}}
+      anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+      {...props}
+    >
+      <MenuItem
+        primaryText="top"
+        containerElement={<Link to="/" />}
+      />
+      <MenuItem
+        primaryText="search"
+        containerElement={<Link to="/search" />}
+      />
+      <MenuItem
+        primaryText="HelloWorld"
+        containerElement={<Link to="/helloworld" />}
+      />
+    </IconMenu>
+    }
+  />
+);
 
 class Header extends React.Component {
   render() {
     return (
       <MuiThemeProvider muiTheme={muiTheme}>
         <div>
-            <header>
-              <AppBar
-                title='React + Redux Sample'
-              />
-            </header>
+          <header>
+            <AppBarIconMenu />
+          </header>
         </div>
       </MuiThemeProvider>
     );
