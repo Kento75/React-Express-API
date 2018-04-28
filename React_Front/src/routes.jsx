6df3a5d107936098e24362e45d5b9205861c40ee
Kento75 <kentonet75@gmail.com>
2018-04-29 05:38:08 +0900

    フォルダ分け+登録ロジック作成

--- React_Front/src/routes.jsx
+++ React_Front/src/routes.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { Route } from 'react-router';
 
 import App from './components/App';
-import HelloWorld from './components/HelloWorld';
-import SearchContainer from './containers/SearchContainer';
+import HelloWorld from './components/helloworld/HelloWorld';
+import SearchContainer from './containers/search/SearchContainer';
 
 export default (store) => {
 
@@ -11,6 +11,7 @@ export default (store) => {
     <Route path="/" component={App}>
       <Route path="helloworld" component={HelloWorld}/>
       <Route path="search" component={SearchContainer}/>
+      <Route path="Create" component={CreateContainer}/>
     </Route>
   );
 };
